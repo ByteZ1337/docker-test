b78421536d537e3caa0bb6037c2e23aba72f78d0
ByteZ1337 <0xbytez@gmail.com>
2023-05-02 21:54:59 +0200

    a

--- .github/scripts/build-rust.sh
+++ .github/scripts/build-rust.sh
@@ -2,3 +2,7 @@ cd rust-test
 
 cargo build --target x86_64-pc-windows-gnu --release
 cargo build --target x86_64-unknown-linux-gnu --release
+
+mkdirs ../bin
+cp ./target/x86_64-pc-windows-gnu/release/docker_test.dll ../bin/docker_test.dll
+cp ./target/x86_64-unknown-linux-gnu/release/libdocker_test.so ../bin/libdocker_test.so
