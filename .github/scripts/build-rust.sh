2bcca802ff7d6712144335085240b9b5e242cbc1
ByteZ1337 <0xbytez@gmail.com>
2023-05-02 21:59:54 +0200

    Update build-rust.sh

--- .github/scripts/build-rust.sh
+++ .github/scripts/build-rust.sh
@@ -3,6 +3,6 @@ cd rust-test
 cargo build --target x86_64-pc-windows-gnu --release
 cargo build --target x86_64-unknown-linux-gnu --release
 
-mkdirs ../bin
-cp ./target/x86_64-pc-windows-gnu/release/docker_test.dll ../bin/docker_test.dll
-cp ./target/x86_64-unknown-linux-gnu/release/libdocker_test.so ../bin/libdocker_test.so
+mkdir /app/bin
+cp ./target/x86_64-pc-windows-gnu/release/docker_test.dll /app/bin/docker_test.dll
+cp ./target/x86_64-unknown-linux-gnu/release/libdocker_test.so /app/bin/libdocker_test.so
