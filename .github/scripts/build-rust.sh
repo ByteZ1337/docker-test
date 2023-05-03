6193bfe9244dbe31f2095dddeac6f54b35636f92
ByteZ1337 <0xbytez@gmail.com>
2023-05-03 08:56:23 +0200

    Update build-rust.sh

--- .github/scripts/build-rust.sh
+++ .github/scripts/build-rust.sh
@@ -6,10 +6,10 @@ cross build --target x86_64-pc-windows-gnu --release
 cross build --target x86_64-unknown-linux-gnu --release
 cross build --target aarch64-unknown-linux-gnu --release
 
-mkdir /app/bin
-cp ./target/x86_64-pc-windows-gnu/release/docker_test.dll /app/bin/docker_test_x86_64-pc-windows-gnu.dll
-cp ./target/x86_64-unknown-linux-gnu/release/libdocker_test.so /app/bin/libdocker_test_x86_64-unknown-linux-gnu.so
-cp ./target/aarch64-unknown-linux-gnu/release/libdocker_test.so /app/bin/libdocker_test_aarch64-unknown-linux-gnu.so
+mkdir ./../bin
+cp ./target/x86_64-pc-windows-gnu/release/docker_test.dll ./../bin/docker_test_x86_64-pc-windows-gnu.dll
+cp ./target/x86_64-unknown-linux-gnu/release/libdocker_test.so ./../bin/libdocker_test_x86_64-unknown-linux-gnu.so
+cp ./target/aarch64-unknown-linux-gnu/release/libdocker_test.so ./../bin/libdocker_test_aarch64-unknown-linux-gnu.so
 
 echo "----------------------------------------"
 echo "Target:"
