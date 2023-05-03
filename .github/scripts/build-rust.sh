33d357b8aa50d777dd842113fc222afd48794663
ByteZ1337 <0xbytez@gmail.com>
2023-05-03 08:49:48 +0200

    Update build-rust.sh

--- .github/scripts/build-rust.sh
+++ .github/scripts/build-rust.sh
@@ -2,9 +2,9 @@ cargo install cross --git https://github.com/cross-rs/cross
 
 cd rust-test
 
-cross build --target x86_64-pc-windows-gnu --release -- -C lto
-cross build --target x86_64-unknown-linux-gnu --release -- -C lto
-cross build --target aarch64-unknown-linux-gnu --release -- -C lto
+cross build --target x86_64-pc-windows-gnu --release
+cross build --target x86_64-unknown-linux-gnu --release
+cross build --target aarch64-unknown-linux-gnu --release
 
 mkdir /app/bin
 cp ./target/x86_64-pc-windows-gnu/release/docker_test.dll /app/bin/docker_test_x86_64-pc-windows-gnu.dll
